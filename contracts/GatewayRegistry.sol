a91d5e7d6b9bbb4b37ac02b1ba2f90700b6a607e
Zied <26070035+zguesmi@users.noreply.github.com>
2026-01-09 16:17:05 +0100

    feat: Check initial addresses

--- contracts/GatewayRegistry.sol
+++ contracts/GatewayRegistry.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.0;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {AccessControlDefaultAdminRulesUpgradeable} from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol";
+import {IGatewayRegistry} from "./interfaces/IGatewayRegistry.sol";
 
-contract GatewayRegistry is UUPSUpgradeable, AccessControlDefaultAdminRulesUpgradeable {
+/**
+ * TODO
+ */
+contract GatewayRegistry is IGatewayRegistry, UUPSUpgradeable, AccessControlDefaultAdminRulesUpgradeable {
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
-    bytes32 public constant GATEWAY_ROLE = keccak256("GATEWAY_ROLE");
 
     /**
      * @custom:oz-upgrades-unsafe-allow constructor
@@ -21,6 +24,9 @@ contract GatewayRegistry is UUPSUpgradeable, AccessControlDefaultAdminRulesUpgra
      * @param initialUpgrader Initial upgrader address
      */
     function initialize(address initialAdmin, address initialUpgrader) public initializer {
+        if (initialAdmin == address(0) || initialUpgrader == address(0)) {
+            revert InvalidZeroAddress();
+        }
         __UUPSUpgradeable_init();
         __AccessControlDefaultAdminRules_init(0, initialAdmin);
         _grantRole(UPGRADER_ROLE, initialUpgrader);
---
+++ contracts/interfaces/IGatewayRegistry.sol
@@ -0,0 +1,7 @@
+// generate contract
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+interface IGatewayRegistry {
+    error InvalidZeroAddress();
+}
