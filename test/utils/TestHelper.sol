fa0ccd7020b0c855350c3050bbc85a9d061952ef
Le-Caignec <robinlecaignec@yahoo.fr>
2026-01-27 18:12:30 +0100

    refactor: Update handle generation logic in TEEComputeManager and improve comments

--- test/utils/TestHelper.sol
+++ test/utils/TestHelper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {Vm} from "forge-std/src/Vm.sol";
+import {Vm} from "forge-std/Vm.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {ACL} from "../../contracts/ACL.sol";
 import {TEEComputeManager} from "../../contracts/TEEComputeManager.sol";
