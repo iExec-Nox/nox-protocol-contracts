0c47daf0a107438ee6ef2f9e5e478a1066e8d782
Zied <26070035+zguesmi@users.noreply.github.com>
2026-01-30 10:47:43 +0100

    test: Init offchain services mock

--- test/integration/ConfidentialToken.test.ts
+++ test/integration/ConfidentialToken.test.ts
@@ -1,26 +1,53 @@
-import { describe, it } from "node:test";
-import { loadFixture } from "../utils/fixture.js";
-import { concatHex, toHex } from "viem";
-import connection from "../../scripts/utils/hardhat-connection-singleton.js";
+import { after, describe, it } from "node:test";
 import assert from "node:assert";
+import { concatHex, toHex } from "viem";
+import { loadFixture } from "../utils/fixture.js";
+import connection from "../../scripts/utils/hardhat-connection-singleton.js";
+import { createHandleAndProof, start, stop } from "./OffChainServicesMock.js";
 
 // Random handle with chain id 31337 (0x00007a69) of type uint256 (3) and version 0
 const handle = "0xabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd00007a690300";
 
-describe("[e2e] ConfidentialToken", function () {
+describe.only("[e2e] ConfidentialToken", function () {
+    after(async function () {
+        await stop();
+    });
+
     it("Should transfer tokens between two addresses", async function () {
         const { teeComputeManager, admin, wallet1: user, gateway } = await loadFixture();
+        const client = await connection.viem.getPublicClient();
+        // Start the mock gateway service here.
+        await start(teeComputeManager.address);
         const confidentialTokenMock = await connection.viem.deployContract("ConfidentialTokenMock", [
             teeComputeManager.address,
         ]);
         const initialAdminBalance = await confidentialTokenMock.read.confidentialBalanceOf([admin.account.address]);
         const totalSupply = await confidentialTokenMock.read.confidentialTotalSupply();
         console.log("Total supply:", totalSupply);
-        assert.equal(totalSupply, "0x8feb32af145f911a6dedaec518831684917b0d1f72db724025ec131a587ed576");
+        assert.equal(totalSupply, "0x00000000000000000000000000000000000000000000000f424000007a690300");
         console.log("Initial balance:", initialAdminBalance);
-        assert.equal(initialAdminBalance, "0x8feb32af145f911a6dedaec518831684917b0d1f72db724025ec131a587ed576");
+        assert.equal(initialAdminBalance, "0x00000000000000000000000000000000000000000000000f424000007a690300");
         const initialUserBalance = await confidentialTokenMock.read.confidentialBalanceOf([user.account.address]);
         console.log("Initial user balance:", initialUserBalance);
         assert.equal(initialUserBalance, toHex(0, { size: 32 }));
+        // Transfer some tokens from admin to user
+        const { handle, proof } = await createHandleAndProof(
+            1000,
+            3,
+            await admin.getChainId(),
+            admin.account.address,
+            confidentialTokenMock.address,
+            gateway,
+            teeComputeManager.address,
+        );
+        const tx = await confidentialTokenMock.write.confidentialTransfer([user.account.address, handle, proof], {
+            account: admin.account,
+        });
+        await client.waitForTransactionReceipt({ hash: tx });
+        const finalAdminBalance = await confidentialTokenMock.read.confidentialBalanceOf([admin.account.address]);
+        const finalUserBalance = await confidentialTokenMock.read.confidentialBalanceOf([user.account.address]);
+        // assert.equal(decrypt(finalAdminBalance), 1_000_000_000 - 1000);
+        // assert.equal(decrypt(finalUserBalance), 1000);
+        // await stop();
     });
 });
---
+++ test/integration/OffChainServicesMock.ts
@@ -0,0 +1,115 @@
+// A mock service to simulate the Gateway and the Runner.
+
+import { randomBytes } from "crypto";
+import connection from "../../scripts/utils/hardhat-connection-singleton.js";
+import { concatHex, Log, parseAbiItem, PrivateKeyAccount, toHex, WatchEventReturnType } from "viem";
+
+const AddEvent = parseAbiItem(
+    "event Add(address indexed caller,bytes32 leftHandOperand,bytes32 rightHandOperand,bytes32 result)",
+);
+
+let running = false;
+let stopGatewayService: WatchEventReturnType;
+
+export async function start(teeComputeManagerAddress: `0x${string}`) {
+    if (running) {
+        throw new Error("Mock services are already running");
+    }
+    running = true;
+    stopGatewayService = await startGateway(teeComputeManagerAddress);
+}
+
+export async function stop() {
+    if (!running) {
+        throw new Error("Mock services are not running");
+    }
+    running = false;
+    stopGatewayService();
+}
+
+async function startGateway(teeComputeManagerAddress: `0x${string}`) {
+    console.log("📡 Gateway service mock started");
+    const client = await connection.viem.getPublicClient();
+    const unwatch = client.watchEvent({
+        address: teeComputeManagerAddress,
+        event: AddEvent,
+        onLogs: (logs) => {
+            console.log("received event");
+            for (const log of logs) {
+                processEvent(log);
+            }
+        },
+        onError(error) {
+            console.error("❌ Event listener error", error);
+        },
+    });
+    process.on("SIGINT", () => {
+        unwatch();
+        process.exit();
+    });
+    return unwatch;
+}
+
+function processEvent(log: Log<bigint, number, false, typeof AddEvent>) {
+    const { caller, leftHandOperand, rightHandOperand, result } = log.args;
+    console.log("🧾 Add event");
+    console.log({
+        caller,
+        leftHandOperand,
+        rightHandOperand,
+        result,
+        txHash: log.transactionHash,
+        blockNumber: log.blockNumber,
+    });
+}
+
+export function createHandle(chainId: number, teeType: number): `0x${string}` {
+    const preHandle = toHex(randomBytes(26));
+    const chainIdBytes = toHex(chainId, { size: 4 });
+    const teeTypeByte = toHex(teeType, { size: 1 });
+    const versionByte = toHex(0, { size: 1 });
+    return concatHex([preHandle, chainIdBytes, teeTypeByte, versionByte]);
+}
+
+export async function createHandleAndProof(
+    value: number,
+    teeType: number,
+    chainId: number,
+    userAddress: `0x${string}`,
+    appAddress: `0x${string}`,
+    gateway: PrivateKeyAccount,
+    teeComputeManagerAddress: `0x${string}`,
+): Promise<{ handle: `0x${string}`; proof: `0x${string}` }> {
+    const createdAt = BigInt(Math.floor(Date.now() / 1000)); // in seconds
+    const handle = createHandle(Number(chainId), teeType);
+
+    const domain = {
+        name: "TEEComputeManager",
+        version: "1",
+        chainId,
+        verifyingContract: teeComputeManagerAddress,
+    } as const;
+    const types = {
+        HandleProof: [
+            { name: "handle", type: "bytes32" },
+            { name: "owner", type: "address" },
+            { name: "app", type: "address" },
+            { name: "createdAt", type: "uint256" },
+        ],
+    } as const;
+    const message = {
+        handle,
+        owner: userAddress,
+        app: appAddress,
+        createdAt,
+    } as const;
+
+    const signature = await gateway.signTypedData({
+        domain,
+        types,
+        primaryType: "HandleProof",
+        message,
+    });
+    const proof = concatHex([userAddress, appAddress, toHex(createdAt, { size: 32 }), signature]);
+    return { handle, proof };
+}
