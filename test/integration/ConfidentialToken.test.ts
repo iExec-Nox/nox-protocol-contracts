ad6b28651e6ebd146d2914be4986b4a92f1cd021
Zied <26070035+zguesmi@users.noreply.github.com>
2026-01-30 18:13:34 +0100

    test: Fix integration test

--- test/integration/ConfidentialToken.test.ts
+++ test/integration/ConfidentialToken.test.ts
@@ -1,59 +1,79 @@
 import { after, beforeEach, describe, it } from "node:test";
 import assert from "node:assert";
-import { concatHex, toHex } from "viem";
+import { zeroHash } from "viem";
 import { loadFixture } from "../utils/fixture.js";
 import connection from "../../scripts/utils/hardhat-connection-singleton.js";
 import { OffChainServices } from "./OffChainServicesMock.js";
 
-// Random handle with chain id 31337 (0x00007a69) of type uint256 (3) and version 0
-const handle = "0xabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd00007a690300";
-
-let teeComputeManager;
-let admin, user;
-let gateway;
+let teeComputeManager: Awaited<ReturnType<typeof loadFixture>>["teeComputeManager"];
+let admin: Awaited<ReturnType<typeof loadFixture>>["admin"];
+let user: Awaited<ReturnType<typeof loadFixture>>["wallet1"];
+let gateway: Awaited<ReturnType<typeof loadFixture>>["gateway"];
 let offChainServices: OffChainServices;
+let client: Awaited<ReturnType<typeof connection.viem.getPublicClient>>;
 
-describe.only("[e2e] ConfidentialToken", function () {
+describe.only("[IT] ConfidentialToken", function () {
     beforeEach(async function () {
         ({ teeComputeManager, admin, wallet1: user, gateway } = await loadFixture());
+        // Start the off-chain services mock.
         offChainServices = new OffChainServices(teeComputeManager.address, gateway, await admin.getChainId());
         await offChainServices.start();
+        client = await connection.viem.getPublicClient();
     });
 
     after(async function () {
+        // Make sure tests always exit even if something fails.
         await offChainServices.stop();
     });
 
     it("Should transfer tokens between two addresses", async function () {
-        const client = await connection.viem.getPublicClient();
-        // Start the mock gateway service here.
+        //
+        // Deploy the ConfidentialTokenMock contract.
+        //
+        const totalSupply = 1_000_000_000n;
         const confidentialTokenMock = await connection.viem.deployContract("ConfidentialTokenMock", [
+            totalSupply,
             teeComputeManager.address,
         ]);
+        await _waitForEventProcessing();
+        //
+        // Check initial balances
+        //
+        const initialTotalSupply = await confidentialTokenMock.read.confidentialTotalSupply();
         const initialAdminBalance = await confidentialTokenMock.read.confidentialBalanceOf([admin.account.address]);
-        const totalSupply = await confidentialTokenMock.read.confidentialTotalSupply();
-        console.log("Total supply:", totalSupply);
-        assert.equal(totalSupply, "0x00000000000000000000000000000000000000000000000f424000007a690300");
-        console.log("Initial balance:", initialAdminBalance);
-        assert.equal(initialAdminBalance, "0x00000000000000000000000000000000000000000000000f424000007a690300");
         const initialUserBalance = await confidentialTokenMock.read.confidentialBalanceOf([user.account.address]);
-        console.log("Initial user balance:", initialUserBalance);
-        assert.equal(initialUserBalance, toHex(0, { size: 32 }));
+        assert.equal(offChainServices.decrypt(initialTotalSupply), totalSupply);
+        assert.equal(offChainServices.decrypt(initialAdminBalance), totalSupply);
+        assert.equal(initialUserBalance, zeroHash); // User balance not defined yet.
+        //
         // Transfer some tokens from admin to user
+        //
+        const amount = 1000n;
         const { handle, proof } = await offChainServices.createHandleAndProof(
-            1000,
-            3,
+            amount,
+            3, // TEEType.uint256
             admin.account.address,
             confidentialTokenMock.address,
         );
-        const tx = await confidentialTokenMock.write.confidentialTransfer([user.account.address, handle, proof], {
+        const txHash = await confidentialTokenMock.write.confidentialTransfer([user.account.address, handle, proof], {
             account: admin.account,
         });
-        await client.waitForTransactionReceipt({ hash: tx });
+        await _waitForEventProcessing();
+        // Check balances after transfer.
+        const finalTotalSupply = await confidentialTokenMock.read.confidentialTotalSupply();
         const finalAdminBalance = await confidentialTokenMock.read.confidentialBalanceOf([admin.account.address]);
         const finalUserBalance = await confidentialTokenMock.read.confidentialBalanceOf([user.account.address]);
-        // assert.equal(decrypt(finalAdminBalance), 1_000_000_000 - 1000);
-        // assert.equal(decrypt(finalUserBalance), 1000);
-        // await stop();
+        assert.equal(offChainServices.decrypt(finalTotalSupply), totalSupply);
+        assert.equal(offChainServices.decrypt(finalUserBalance), amount);
+        assert.equal(offChainServices.decrypt(finalAdminBalance), totalSupply - amount);
     });
+
+    /**
+     * Waits for the transaction to be mined and for event processing to be done.
+     */
+    async function _waitForEventProcessing() {
+        // Wait a bit to ensure event processing is done.
+        // TODO enhance this.
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
 });
--- test/integration/OffChainServicesMock.ts
+++ test/integration/OffChainServicesMock.ts
@@ -2,9 +2,13 @@
 
 import { randomBytes } from "crypto";
 import connection from "../../scripts/utils/hardhat-connection-singleton.js";
-import { concatHex, Log, parseAbiItem, PrivateKeyAccount, toHex, WatchEventReturnType } from "viem";
+import { concatHex, parseAbiItem, PrivateKeyAccount, toHex, WatchEventReturnType } from "viem";
+
+// Can be used for debugging.
+const printLogs = true;
 
 const eventsToWatch = [
+    "event PlaintextToEncrypted(address indexed caller,uint256 plainText,uint8 toType,bytes32 result)",
     "event Add(address indexed caller,bytes32 leftHandOperand,bytes32 rightHandOperand,bytes32 result)",
     "event Sub(address indexed caller,bytes32 leftHandOperand,bytes32 rightHandOperand,bytes32 result)",
 ].map((e) => parseAbiItem(e));
@@ -13,7 +17,7 @@ export class OffChainServices {
     private teeComputeManagerAddress: `0x${string}`;
     private gateway: PrivateKeyAccount;
     private chainId: number;
-    private handleToValueMap: Map<`0x${string}`, number> = new Map();
+    private handleToValueMap: Map<`0x${string}`, bigint> = new Map();
     private running = false;
     private stopGatewayService: WatchEventReturnType = () => {};
 
@@ -32,6 +36,7 @@ export class OffChainServices {
         }
         this.running = true;
         this.stopGatewayService = await this._startGateway();
+        _print("Mock services started");
     }
 
     /**
@@ -43,20 +48,22 @@ export class OffChainServices {
         }
         this.running = false;
         this.stopGatewayService();
+        _print("Mock services stopped");
     }
 
     /**
+     * Simulates the Gateway service.
      * Generates a handle and its corresponding proof for a given value.
      * It saves the value associated with the handle in an internal map.
      */
     async createHandleAndProof(
-        value: number,
+        value: bigint,
         teeType: number,
         userAddress: `0x${string}`,
         appAddress: `0x${string}`,
     ): Promise<{ handle: `0x${string}`; proof: `0x${string}` }> {
         const createdAt = BigInt(Math.floor(Date.now() / 1000)); // in seconds
-        const handle = this.createHandle(value, teeType);
+        const handle = this._createAndSaveHandle(value, teeType);
         const domain = {
             name: "TEEComputeManager",
             version: "1",
@@ -88,45 +95,88 @@ export class OffChainServices {
     }
 
     /**
-     * Generates a handle for a given value.
+     * Simulates decryption.
      */
-    createHandle(value: number, teeType: number): `0x${string}` {
+    decrypt(handle: `0x${string}`): bigint {
+        const value = this.handleToValueMap.get(handle);
+        if (!value) {
+            throw new Error(`Handle not found: ${handle}`);
+        }
+        return value;
+    }
+
+    /**
+     * Generates a handle for a given value and saves it in the internal map.
+     */
+    private _createAndSaveHandle(value: bigint, teeType: number): `0x${string}` {
         const preHandle = toHex(randomBytes(26));
         const chainIdBytes = toHex(this.chainId, { size: 4 });
         const teeTypeByte = toHex(teeType, { size: 1 });
         const versionByte = toHex(0, { size: 1 });
         const handle = concatHex([preHandle, chainIdBytes, teeTypeByte, versionByte]);
-        this.handleToValueMap.set(handle, value);
+        this._saveHandle(handle, value);
         return handle;
     }
 
-    async _startGateway() {
-        console.log("📡 Gateway service mock started");
+    private async _startGateway() {
         const client = await connection.viem.getPublicClient();
         const unwatch = client.watchEvent({
             address: this.teeComputeManagerAddress,
             events: eventsToWatch,
-            onLogs: (logs) => this._processEvent(logs),
+            // pollingInterval: 10,
+            onLogs: (logs) => this._processEvents(logs),
             onError(error) {
-                console.error("❌ Event listener error", error);
+                console.error("❌ Event listener error");
+                console.error(error);
             },
         });
         return unwatch;
     }
 
-    _processEvent(logs: Log[]) {
-        for (const log of logs) {
-            // this._processEvent(log);
-            const { caller, leftHandOperand, rightHandOperand, result } = log.args;
-            console.log("🧾 Add event");
-            console.log({
-                caller,
-                leftHandOperand,
-                rightHandOperand,
-                result,
-                txHash: log.transactionHash,
-                blockNumber: log.blockNumber,
-            });
+    /**
+     * Simulates the off-chain runner.
+     */
+    private _processEvents(eventLogs: any[]) {
+        _print(`Gateway processing ${eventLogs.length} event(s): ${eventLogs.map((e) => e.eventName).join(", ")}`);
+        for (const log of eventLogs) {
+            const eventName = log.eventName;
+            _print(`Processing event: ${eventName}`);
+            if (eventName === "PlaintextToEncrypted") {
+                const { plainText, result } = log.args as { plainText: bigint; result: `0x${string}` };
+                _print(`(e) PlaintextToEncrypted: ${result} -> ${plainText}`);
+                this._saveHandle(result, plainText);
+            } else if (eventName === "Add") {
+                const { leftHandOperand, rightHandOperand, result } = log.args as {
+                    leftHandOperand: bigint;
+                    rightHandOperand: bigint;
+                    result: `0x${string}`;
+                };
+                const addValue = leftHandOperand + rightHandOperand;
+                _print(`(e) Add: ${leftHandOperand} + ${rightHandOperand} = ${addValue} -> ${result}`);
+                this._saveHandle(result, addValue);
+            } else if (eventName === "Sub") {
+                const { leftHandOperand, rightHandOperand, result } = log.args as {
+                    leftHandOperand: bigint;
+                    rightHandOperand: bigint;
+                    result: `0x${string}`;
+                };
+                const subValue = leftHandOperand - rightHandOperand;
+                _print(`(e) Sub: ${leftHandOperand} - ${rightHandOperand} = ${subValue} -> ${result}`);
+                this._saveHandle(result, subValue);
+            } else {
+                throw new Error(`Unknown event: ${eventName}`);
+            }
         }
+    }
+
+    private _saveHandle(handle: `0x${string}`, value: bigint) {
+        this.handleToValueMap.set(handle, value);
+        _print(`Saved handle: ${handle} -> value: ${value}`);
+    }
+}
+
+function _print(message: string) {
+    if (printLogs) {
+        console.log(`[Debug] ${message}`);
     }
 }
