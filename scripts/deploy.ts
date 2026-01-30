a2235e3cf92f41d5eff53ed2dbcf7da62d9a7249
Zied <26070035+zguesmi@users.noreply.github.com>
2026-01-30 17:10:56 +0100

    chore: Clean deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,37 +11,33 @@ import connection from "./utils/hardhat-connection-singleton.js";
 
 /**
  * Deployment function to be imported in other scripts.
- * @param log whether to print deployment messages or not, useful to get clean test logs
+ * @param printLogs whether to print deployment messages or not, useful to get clean test logs
  * @returns Viem contract instances for the deployed proxy contracts
  */
-export async function deploy(log = true) {
+export async function deploy(printLogs = true) {
     const { viem } = connection;
     const publicClient = await viem.getPublicClient();
     const chainConfig = config[connection.networkName];
     if (!chainConfig) {
         throw new Error(`No chain config found for network: ${connection.networkName}`);
     }
-    if (log) {
-        console.log(`Deploying to network: ${connection.networkName} (chainId: ${connection.networkConfig.chainId})`);
-        console.log(`Chain config:`, chainConfig);
-    }
+    _print(printLogs, `Deploying to network: ${connection.networkName} (chainId: ${connection.networkConfig.chainId})`);
+    _print(printLogs, `Chain config: ${JSON.stringify(chainConfig)}`);
     // Deploy TEEComputeManager.
     const { proxy: teeComputeManagerProxy } = await connection.ignition.deploy(TEEComputeManager, {
         deploymentId: connection.networkName,
-        displayUi: log,
+        displayUi: printLogs,
         parameters: {
             TEEComputeManager: {
                 initialOwner: chainConfig.initialOwner,
             },
         },
     });
-    if (log) {
-        console.log(`TEEComputeManager: ${teeComputeManagerProxy.address}`);
-    }
+    _print(printLogs, `TEEComputeManager: ${teeComputeManagerProxy.address}`);
     // Deploy ACL
     const { proxy: aclProxy } = await connection.ignition.deploy(ACL, {
         deploymentId: connection.networkName,
-        displayUi: log,
+        displayUi: printLogs,
         parameters: {
             ACL: {
                 initialOwner: chainConfig.initialOwner,
@@ -49,9 +45,7 @@ export async function deploy(log = true) {
             },
         },
     });
-    if (log) {
-        console.log(`ACL: ${aclProxy.address}`);
-    }
+    _print(printLogs, `ACL: ${aclProxy.address}`);
     // Update ACL address in TEEComputeManager.
     const teeComputeManager = await viem.getContractAt("TEEComputeManager", teeComputeManagerProxy.address);
     const setAclTxHash = await teeComputeManager.write.setAcl([aclProxy.address]);
@@ -75,4 +69,10 @@ function _isHardhatRunCommand() {
     // When running `hardhat run scripts/deploy.ts`, the argv looks like:
     // [ "/.../bin/node", "/.../cli.js", "run", "scripts/deploy.ts"];
     return process.argv.length >= 4 && process.argv[2] === "run" && process.argv[3].includes("scripts/deploy.ts");
+}
+
+function _print(shouldPrint: boolean, message: string) {
+    if (shouldPrint) {
+        console.log(message);
+    }
 }
