2c2dd972745a9d2ed54bad458ba02f372811f5f2
Le-Caignec <robinlecaignec@yahoo.fr>
2026-02-05 16:37:04 +0100

    feat: change TEE_COMPUTE_MANAGER and ACL_ADDRESS to internal constants in TEEPrimitives

--- contracts/lib/TEEPrimitives.sol
+++ contracts/lib/TEEPrimitives.sol
@@ -20,8 +20,8 @@ import "encrypted-types/EncryptedTypes.sol";
  */
 library TEEPrimitives {
     // TODO: Update these addresses after deploying with the production salt.
-    address public constant TEE_COMPUTE_MANAGER = 0x029Ab6663e4F73477494082EB88915ea74Df5e83;
-    address public constant ACL_ADDRESS = 0x310163c93461AB5c6445044B15B0DA1784b595FB;
+    address internal constant TEE_COMPUTE_MANAGER = 0x029Ab6663e4F73477494082EB88915ea74Df5e83;
+    address internal constant ACL_ADDRESS = 0x310163c93461AB5c6445044B15B0DA1784b595FB;
 
     // ============ Trivial Encryption Functions ============
 
