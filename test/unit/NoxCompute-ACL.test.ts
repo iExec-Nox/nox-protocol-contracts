8719a31decdd0d0e5645ada7703d192e26b6b245
Zied <26070035+zguesmi@users.noreply.github.com>
2026-02-24 17:16:09 +0100

    chore: Clean

--- test/unit/NoxCompute-ACL.test.ts
+++ test/unit/NoxCompute-ACL.test.ts
@@ -4,7 +4,6 @@ import { encodeAbiParameters, keccak256, padHex, toHex } from "viem";
 import { loadFixture } from "../utils/fixture.ts";
 import connection from "../../scripts/utils/hardhat-connection-singleton.ts";
 
-// TODO rename to NoxCompute.test.ts.
 describe("NoxCompute-ACL", function () {
     describe("Transient & Persistent permissions", function () {
         it("Should clear transient permissions after transaction while persistent remain", async function () {
