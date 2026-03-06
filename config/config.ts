f4b96f829a8ea68b86a438f03fe230f8b6ff1e84
Zied Guesmi <26070035+zguesmi@users.noreply.github.com>
2026-03-06 11:03:29 +0100

    feat: Remove public function from sdk library (#76)

--- config/config.ts
+++ config/config.ts
@@ -13,13 +13,13 @@ export default {
     },
     arbitrumSepolia: {
         chainId: 421614,
-        initialOwner: "0x0bcEAC5cdb4f6390c470972dCBDbeefdD88cfB8f", // TODO: Replace before real deployment
+        initialOwner: "0x0bcEAC5cdb4f6390c470972dCBDbeefdD88cfB8f",
         kmsPublicKey: undefined, // Defined in GitHub environment.
     },
     tenderlyArbitrumSepolia: {
         chainId: 421614, // Same chainId as Arbitrum Sepolia
-        initialOwner: "0x9990cfb1feb7f47297f54bef4d4ebedf6c5463a3", // TODO: Replace before real deployment
-        kmsPublicKey: "0x026f0005c5c3807e69dcbe52a97ca55aa26c8655999b5a81f5098666cd7dd5d1f6", // TODO: Replace before real deployment
+        initialOwner: "0x9990cfb1feb7f47297f54bef4d4ebedf6c5463a3", // TODO Replace this
+        kmsPublicKey: "0x026f0005c5c3807e69dcbe52a97ca55aa26c8655999b5a81f5098666cd7dd5d1f6",
     },
 } as {
     [network: string]: {
