885a61f1e70348be7dbf67bda25404e30f37fca5
Zied <26070035+zguesmi@users.noreply.github.com>
2026-04-09 10:36:08 +0200

    chore: Fix tests

--- test/unit/shared/TypeUtils.t.sol
+++ test/unit/shared/TypeUtils.t.sol
@@ -11,42 +11,68 @@ import {
 import {TestHelper} from "../../utils/TestHelper.sol";
 
 contract TypeUtilsTest is Test {
+    TypeUtilsMock private typeUtilsMock = new TypeUtilsMock();
     TEEType[4] supportedTypes = [TEEType.Uint16, TEEType.Uint256, TEEType.Int16, TEEType.Int256];
 
     function test_TypesLength() public pure {
-        assertEq(uint8(type(TEEType).max), 100);
+        assertEq(uint8(type(TEEType).max), 99);
     }
 
     // ============ typeOf ============
 
     function test_TypeOf_ReturnsType() public {
         bytes32 handle = TestHelper.createHandle(TEEType.Uint256);
-        assertEq(uint8(TypeUtils.typeOf(handle)), uint8(TEEType.Uint256));
+        assertEq(uint8(typeUtilsMock.typeOf(handle)), uint8(TEEType.Uint256));
     }
 
     // ============ validateArithmeticType ============
 
     function test_ValidateArithmeticType() public view {
         for (uint256 i = 0; i < supportedTypes.length; i++) {
-            TypeUtils.validateArithmeticType(supportedTypes[i]);
+            typeUtilsMock.validateArithmeticType(supportedTypes[i]);
         }
     }
 
     function test_RevertWhen_ValidateArithmeticType_NonArithmeticType() public {
-        for (uint8 i = 0; i < uint8(TEEType.String); i++) {
+        for (uint8 i = 0; i < uint8(TEEType.Uint8); i++) {
             vm.expectRevert(NonArithmeticType.selector);
-            TypeUtils.validateArithmeticType(TEEType(i));
+            typeUtilsMock.validateArithmeticType(TEEType(i));
         }
-        for (uint8 i = uint8(TEEType.Bytes1); i < uint8(type(TEEType).max); i++) {
+        for (uint8 i = uint8(TEEType.Bytes1); i <= uint8(type(TEEType).max); i++) {
             vm.expectRevert(NonArithmeticType.selector);
-            TypeUtils.validateArithmeticType(TEEType(i));
+            typeUtilsMock.validateArithmeticType(TEEType(i));
         }
     }
 
     function test_RevertWhen_ValidateArithmeticType_UnsupportedArithmeticType() public {
-        for (uint8 i = uint8(TEEType.Uint8); i < uint8(TEEType.Int256); i++) {
+        for (uint8 i = uint8(TEEType.Uint8); i <= uint8(TEEType.Int256); i++) {
+            if (_supportedType(TEEType(i))) {
+                continue;
+            }
             vm.expectRevert(UnsupportedArithmeticType.selector);
-            TypeUtils.validateArithmeticType(TEEType(i));
+            typeUtilsMock.validateArithmeticType(TEEType(i));
         }
+    }
+
+    function _supportedType(TEEType teeType) private view returns (bool) {
+        for (uint256 i = 0; i < supportedTypes.length; i++) {
+            if (teeType == supportedTypes[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Mock contract to test revert scenarios of TypeUtils library.
+ */
+contract TypeUtilsMock {
+    function typeOf(bytes32 handle) public pure returns (TEEType) {
+        return TypeUtils.typeOf(handle);
+    }
+
+    function validateArithmeticType(TEEType teeType) public pure {
+        TypeUtils.validateArithmeticType(teeType);
     }
 }
