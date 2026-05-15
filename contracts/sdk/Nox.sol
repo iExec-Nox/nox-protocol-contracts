149f7ae8632b47d6e9ba4f79bc382e80d6dd9964
Le-Caignec <robinlecaignec@yahoo.fr>
2026-05-15 10:29:00 +0200

    fix: update noxComputeContract address for local development chain

--- contracts/sdk/Nox.sol
+++ contracts/sdk/Nox.sol
@@ -34,7 +34,7 @@ library Nox {
         }
         // Local development chain
         if (block.chainid == 31337) {
-            return 0x95980A29a6092B91361b0C4c8a520E524CdB56A8;
+            return 0x5FdeBd6039082E9A403Ed5035dbB8BC24eD166B1;
         }
         revert("Nox: Unsupported chain");
     }
