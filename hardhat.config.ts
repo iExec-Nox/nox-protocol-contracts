957be4ac2a6519bb68b26cb69b5fcd4a02d7fdce
Le-Caignec <robinlecaignec@yahoo.fr>
2026-05-18 08:41:24 +0200

    feat: add configurations for arbitrum and sepolia networks in config and hardhat files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -53,6 +53,13 @@ export default defineConfig({
             chainType: "op",
             allowUnlimitedContractSize: true,
         },
+        tenderlyArbitrumSepolia: {
+            type: "http",
+            chainType: "op",
+            chainId: 421614,
+            url: configVariable("RPC_URL"),
+            accounts: [configVariable("PRIVATE_KEY")],
+        },
         arbitrumSepolia: {
             type: "http",
             chainType: "op",
@@ -60,10 +67,24 @@ export default defineConfig({
             url: configVariable("RPC_URL"),
             accounts: [configVariable("PRIVATE_KEY")],
         },
-        tenderlyArbitrumSepolia: {
+        arbitrum: {
             type: "http",
             chainType: "op",
-            chainId: 421614,
+            chainId: 42161,
+            url: configVariable("RPC_URL"),
+            accounts: [configVariable("PRIVATE_KEY")],
+        },
+        sepolia: {
+            type: "http",
+            chainType: "l1",
+            chainId: 11155111,
+            url: configVariable("RPC_URL"),
+            accounts: [configVariable("PRIVATE_KEY")],
+        },
+        ethereum: {
+            type: "http",
+            chainType: "l1",
+            chainId: 1,
             url: configVariable("RPC_URL"),
             accounts: [configVariable("PRIVATE_KEY")],
         },
