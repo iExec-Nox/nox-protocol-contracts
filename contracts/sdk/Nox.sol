55ac8036bc61ef1f3965e23341c58916834d0665
Zied <26070035+zguesmi@users.noreply.github.com>
2026-05-19 11:56:30 +0200

    chore: Fix tests

--- contracts/sdk/Nox.sol
+++ contracts/sdk/Nox.sol
@@ -34,7 +34,7 @@ library Nox {
         }
         // Local development chain
         if (block.chainid == 31337) {
-            return 0xFe94304FE3E523b00d5dda6df153B6679DF333dD;
+            return 0xBD92B06b7C6F6F9039438d5141E3A116Ee6Cd08c;
         }
         revert("Nox: Unsupported chain");
     }
