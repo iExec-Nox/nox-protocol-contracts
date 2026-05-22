f6136c2b810137b258064bbbe72e060ad1774713
Zied <26070035+zguesmi@users.noreply.github.com>
2026-05-22 12:06:10 +0200

    chore: Revert "chore: update local proxy address"
    This reverts commit 7f46d844219e38a556b5c485000514942d815de5.

--- contracts/sdk/Nox.sol
+++ contracts/sdk/Nox.sol
@@ -31,7 +31,7 @@ library Nox {
         }
         // Local development chain
         if (block.chainid == 31337) {
-            return 0x712dBD68d4490d673E44884d331265e0a46612CE;
+            return 0xea14e78Ffd154213fC7eEE276B9ed9E17197BE33;
         }
         revert("Nox: Unsupported chain");
     }
