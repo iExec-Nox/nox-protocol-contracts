11c2e4043645a31e44cc54de7d90a5de30a67abb
Le-Caignec <robinlecaignec@yahoo.fr>
2026-05-29 14:44:10 +0200

    feat: update config again

--- config/config.ts
+++ config/config.ts
@@ -26,7 +26,7 @@ export default {
     },
     arbitrumSepolia: {
         chainId: 421614,
-        initialAdmin: "0x0bcEAC5cdb4f6390c470972dCBDbeefdD88cfB8f",
+        initialAdmin: "0x40D03906B889AbF57266C2433B0b200177D15664",
         initialUpgrader: "0x6148045F5281af51Dde61E9072bf8a53004a34c0",
         initialPaymentManager: "0x4f511B48A2c62dc8C4Db0F17A2b963b53b18369A",
         kmsPublicKey: "0x02221f1baca89fd75ec26566b9c6003bc793b47affe171a7824279d973f54a2fd9", // Defined in GitHub environment.
@@ -34,7 +34,7 @@ export default {
     },
     sepolia: {
         chainId: 11155111,
-        initialAdmin: "0x0bcEAC5cdb4f6390c470972dCBDbeefdD88cfB8f",
+        initialAdmin: "0xD45e79Cd834427A8fE3f1Ed406C0781c17335eDb",
         initialUpgrader: "0xC0Ed057C8819b2b00e93B8b22Ff61b7e0269D6A5",
         initialPaymentManager: "0x5390B5df9a134a40FE0b57ba0501549B3B8512d8",
         kmsPublicKey: "0x12e1b0794046ef04bfae49938c1293fabbd6614c04862a4059d5e8a0911635c2", // Defined in GitHub environment.
