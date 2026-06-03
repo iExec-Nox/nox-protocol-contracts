e91ce75aff9a97fd5e580cd5dec359dd464944df
Zied <26070035+zguesmi@users.noreply.github.com>
2026-06-03 16:48:05 +0200

    chore: Fix Tenderly upgrade

--- config/config.ts
+++ config/config.ts
@@ -27,10 +27,10 @@ const config: { [network: string]: ChainConfig } = {
     },
     tenderlyArbitrumSepolia: {
         chainId: 421614,
-        initialAdmin: "0x40D03906B889AbF57266C2433B0b200177D15664",
-        initialUpgrader: "0x6148045F5281af51Dde61E9072bf8a53004a34c0",
-        kmsPublicKey: "0x02221f1baca89fd75ec26566b9c6003bc793b47affe171a7824279d973f54a2fd9",
-        gateway: "0xE13191F53671957C8a48A7A3Ff15E16450a1552F",
+        initialAdmin: "0x9990cfb1Feb7f47297F54bef4d4EbeDf6c5463a3",
+        initialUpgrader: "0x9990cfb1Feb7f47297F54bef4d4EbeDf6c5463a3",
+        kmsPublicKey: "0x026f0005c5c3807e69dcbe52a97ca55aa26c8655999b5a81f5098666cd7dd5d1f6",
+        gateway: "0x209ec5303D3dD935d82dff29e4a74507A8def029",
     },
     arbitrumSepolia: {
         chainId: 421614,
