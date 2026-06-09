8825a41f903a0c088171ccb619ced9de7ac5456f
Zied <26070035+zguesmi@users.noreply.github.com>
2026-06-09 15:16:54 +0200

    chore: update local proxy address

--- contracts/sdk/Nox.sol
+++ contracts/sdk/Nox.sol
@@ -27,7 +27,7 @@ library Nox {
     function noxComputeContract() internal view returns (address) {
         // Hardhat local development chain
         if (block.chainid == 31337) {
-            return 0x4679b874E78fEc1Cdc504be7a7ba6E81F9D01b9e;
+            return 0x1ce6d4D7Fc8bCBFDA91B26173927fdBB2D86F8a6;
         }
         // Arbitrum Sepolia
         if (block.chainid == 421614) {
