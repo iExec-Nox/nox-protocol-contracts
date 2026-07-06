1eb42d5a38b36b822087589ed48f94ff14ea607e
Le-Caignec <robinlecaignec@yahoo.fr>
2026-07-06 10:46:40 +0200

    chore: update local proxy address

--- contracts/sdk/Nox.sol
+++ contracts/sdk/Nox.sol
@@ -39,7 +39,7 @@ library Nox {
     function noxComputeContract() internal view returns (address) {
         // Hardhat local development chain
         if (block.chainid == 31337) {
-            return 0x771fA8627BBBf7f143BdA9e153c06F2088D68F01;
+            return 0xFB26B42957E39BD4e165E7B773f0476737295C69;
         }
         // Arbitrum Sepolia
         if (block.chainid == 421614) {
